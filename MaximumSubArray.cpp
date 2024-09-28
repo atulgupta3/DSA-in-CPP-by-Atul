41083ec598cb7a605d98023c9e1075515f70fd5b
Atul Gupta <136560060+venudharakatul@users.noreply.github.com>
2024-09-28 16:28:22 +0530

    Update MaximumSubArray.cpp

--- MaximumSubArray.cpp
+++ MaximumSubArray.cpp
@@ -1,1 +1,34 @@
 Maximum sub Array
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+    int ans = INT_MIN;
+    int sum = 0;
+
+    for (const int num : nums) {
+      sum = max(num, sum + num);
+      ans = max(ans, sum);
+    }
+
+    return ans;
+    }
+};
+
+///////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int currSum=0;
+        int largest=0;
+        for(int i=0;i<nums.size();i++){
+            currSum= currSum+nums[i];
+            if(currSum<0){
+                currSum=0;
+            }
+            largest=max(currSum,largest);
+        }
+        return largest;
+    }
+};
